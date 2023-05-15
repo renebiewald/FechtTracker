2181bbaa4c454eb2546293d40cecc0505028f5d0
Rene Biewald <mail@rene-biewald.de>
2023-05-15 20:07:02 +0200

    Binding Projeket für das Binden der Services vorbereitet

---
+++ 25_Binding/Binding.Gui.App/Bindings.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Binding.Gui.App
+{
+    public class Bindings
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+    }
+}
